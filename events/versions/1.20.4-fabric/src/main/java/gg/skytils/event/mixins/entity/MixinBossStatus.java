23336c80c71b9b35b04d84efd276d9c68441df2d
sychic <47618543+Sychic@users.noreply.github.com>
2024-11-01 09:31:08 -0400

    refactor: preprocess `MixinBossStatus`
    [no ci]

---
+++ events/versions/1.20.4-fabric/src/main/java/gg/skytils/event/mixins/entity/MixinBossStatus.java
@@ -0,0 +1,39 @@
+/*
+ * Skytils - Hypixel Skyblock Quality of Life Mod
+ * Copyright (C) 2020-2024 Skytils
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package gg.skytils.event.mixins.entity;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import gg.skytils.event.EventsKt;
+import gg.skytils.event.impl.entity.BossBarSetEvent;
+import net.minecraft.client.gui.hud.ClientBossBar;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Map;
+
+@Mixin(targets = "net.minecraft.client.gui.hud.BossBarHud$1")
+public class MixinBossStatus {
+    @WrapOperation(method = "add", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
+    public void onSetBossStatus(Map instance, Object uuid, ClientBossBar bossBar, Operation<Object> original) {
+        if (!EventsKt.postCancellableSync(new BossBarSetEvent(bossBar, false))) {
+            original.call(instance, uuid, bossBar);
+        }
+    }
+}
